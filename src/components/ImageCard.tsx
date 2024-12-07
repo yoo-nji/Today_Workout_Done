7aed0445fb071e7c9f10eb644d93dc117a86de93
yoonji <kyjkbw@gmail.com>
2024-12-08 07:59:10 +0900

    feat: 피드 이미지 카드 유저 정보 추가

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,16 +1,19 @@
 import thumbnail from "../assets/images/feed_thumbnail.jpg";
 import iikeIcon from "../assets/like_icon.svg";
 import chatIcon from "../assets/chat_icon.svg";
+import UserProfile from "./UserProfile";
 
 const cardData = {
   title: "카드 제목 한줄 넘어가면 말줄임표 처리가 됩니다",
   like: 4,
   chat: 12,
+  name: "유저네임123",
+  date: "2024.12.07",
 };
 
 export default function ImageCard() {
   return (
-    <>
+    <div className="flex flex-col items-center gap-3">
       {/* 썸네일 */}
       <div
         className="group relative w-[300px] h-[300px] bg-cover bg-center rounded-2xl shadow-lg"
@@ -34,6 +37,20 @@ export default function ImageCard() {
           </div>
         </div>
       </div>
-    </>
+
+      {/* 글 작성자 정보 */}
+      <div className="flex  items-center justify-between w-full px-2">
+        <div className="flex items-center gap-[10px]">
+          <UserProfile
+            BackWidth="w-[40px]"
+            BackHeight="h-[40px]"
+            IconWidth="w-[28px]"
+            IconHeight="h-[28px]"
+          />
+          <div className="text-base font-medium">{cardData.name}</div>
+        </div>
+        <div className="text-base font-light">{cardData.date}</div>
+      </div>
+    </div>
   );
 }
