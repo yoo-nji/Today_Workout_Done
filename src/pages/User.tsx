3a203a0e949621e0897bf69341d2d184f7e5ab34
kangsuyeong <ekdh16@naver.com>
2024-12-09 00:08:12 +0900

    feat : Userpage 라우트연결

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,12 @@
 import { useParams } from "react-router";
+import UserCard from "../components/User/UserCard";
 
 export default function User() {
   const { user_id } = useParams();
-  return <div>User {user_id}</div>;
+  return (
+    <>
+      <div>User {user_id}</div>
+      <UserCard />
+    </>
+  );
 }
