4a0f71960c39520bf9af8b88ecfa5b727ede2b5f
yoongjeonging <yooong.o353@gmail.com>
2024-12-08 17:36:31 +0900

    fix: css, change hr/a tag

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router";
+
 export default function LoginForm() {
   return (
     <>
       <form
         action=""
-        className=" flex flex-col items-center w-150 h-[730px]m-[50px, 35px] bg-[#FFFFFF]"
+        className=" flex flex-col items-center w-[600px] h-[730px] bg-[#FFFFFF]"
       >
-        <div className="mb-[35px]">
+        <div className="mt-[50px] mb-[35px]">
           <h1 className="text-[#265CAC] text-[20px] font-bold">
             <span className="text-[#AC265C] font-extrabold text-[30px]">
               !0
@@ -20,13 +22,11 @@ export default function LoginForm() {
         <input
           type="text"
           placeholder="아이디"
-          required
           className="w-[530px] h-14 border border-[#C2C2C2] rounded-[10px] placeholder-[#B6B6B6] mb-[35px]"
         />
         <input
           type="password"
           placeholder="비밀번호"
-          required
           className="w-[530px] h-14 border border-[#C2C2C2] rounded-[10px] placeholder-[#B6B6B6] mb-[28px]"
         />
 
@@ -36,10 +36,10 @@ export default function LoginForm() {
         <button className="w-[530px] h-[50px] rounded-[20px] bg-[#3B6CB4] font-bold text-[#FFFFFF] mb-6">
           가입하기
         </button>
-        <hr className="w-[530px] mb-10" />
-        <a href="#" className="font-bold text-[#3B6CB4] mb-3">
+        <div className="border-t border-[#B6B6B6] w-[530px] mb-10"></div>
+        <Link to="/" className="font-bold text-[#3B6CB4] mb-3">
           저희 서비스가 궁금하신가요?
-        </a>
+        </Link>
         <button className="w-[530px] h-[50px] border-2 border-[#3B6CB4]  rounded-[20px] bg-[#FFFFFF] text-[#212121]">
           비회원으로 둘러보기
         </button>
