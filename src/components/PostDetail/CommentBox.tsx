7f120a05aa6cff7f9b51bbc9a12938edbe9661dd
yoonji <kyjkbw@gmail.com>
2024-12-11 22:06:46 +0900

    style: 댓글 박스 스타일 수정

--- src/components/PostDetail/CommentBox.tsx
+++ src/components/PostDetail/CommentBox.tsx
@@ -2,9 +2,9 @@ import UserProfile from "../UserProfile";
 import trashIcon from "../../assets/trash.svg";
 export default function CommentBox() {
   return (
-    <div className="flex flex-col py-4 ">
+    <div className="flex flex-col py-5 border-b border-gray-200">
       <div className="flex justify-between ">
-        <div className="flex w-fit">
+        <div className="flex gap-[12px] w-fit items-center">
           <UserProfile
             BackWidth="w-[36px]"
             BackHeight="h-[36px]"
@@ -12,15 +12,15 @@ export default function CommentBox() {
             IconHeight="h-[25px]"
           />
           <div>
-            <p className="text-[13px] mb-[6px] font-bold">강수영</p>
-            <p className="text-xs">2024.12.10</p>
+            <p className="text-[15px] mb-[2px] font-bold">강수영</p>
+            <p className="text-xs font-light">2024.12.10</p>
           </div>
         </div>
-        <button>
+        <button className="w-8 mr-2">
           <img src={trashIcon} alt="trashIcon" />
         </button>
       </div>
-      <div className="mt-3">댓글</div>
+      <div className="mt-3 text-[14px]">댓글 감사합니다</div>
     </div>
   );
 }
