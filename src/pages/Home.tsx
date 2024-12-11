d73066959d98b4da72499a7f3817bbb44ace90e9
kangsuyeong <ekdh16@naver.com>
2024-12-11 17:28:54 +0900

    fix : 로컬스토리지 정보 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import SearchBar from "../components/SearchBar";
 import ImageCard from "../components/ImageCard";
 import Tag from "../components/Tag";
+import { useAuth } from "../stores/authStore";
 
 export default function Home() {
+  const token = useAuth((state) => state.accessToken);
+  console.log(token);
   return (
-    <div className="flex flex-col items-center mt-8 gap-16">
+    <div className="flex flex-col items-center gap-16 mt-8">
       <div className="flex flex-col items-center gap-[30px] w-full px-4">
         <SearchBar
           placeholder="검색어를 입력해 주세요"
@@ -18,7 +21,7 @@ export default function Home() {
       </div>
       {/* 피드 이미지 */}
       <div className="flex items-center">
-        <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 gap-8">
+        <div className="grid gap-8 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2">
           <ImageCard />
           <ImageCard />
           <ImageCard />
