9dd803c4f46a86e45b875ec2e5e480ba0e996ade
yoongjeonging <yooong.o353@gmail.com>
2024-12-11 17:33:39 +0900

    feat: load 오운완채널 posts

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,38 +1,40 @@
 import thumbnail from "../assets/images/feed_thumbnail.jpg";
-import iikeIcon from "../assets/like_icon.svg";
+import likeIcon from "../assets/like_icon.svg";
 import chatIcon from "../assets/chat_icon.svg";
 import UserProfile from "./UserProfile";
 
-const cardData = {
-  title: "카드 제목 한줄 넘어가면 말줄임표 처리가 됩니다",
-  like: 4,
-  chat: 12,
-  name: "유저네임123",
-  date: "2024.12.07",
-};
+export default function ImageCard({
+  image,
+  title,
+  likes,
+  comments,
+  author,
+  updatedAt,
+}: PostType) {
+  const update = new Date(updatedAt);
+  const date = update.toLocaleDateString("ko-KR").slice(0, -1);
 
-export default function ImageCard() {
   return (
     <div className="flex flex-col items-center gap-3">
       {/* 썸네일 */}
       <div
-        className="group relative w-[250px] h-[250px] bg-cover bg-center rounded-2xl shadow-lg"
-        style={{ backgroundImage: `url(${thumbnail})` }}
+        className="group relative w-[300px] h-[300px] bg-cover bg-center rounded-2xl shadow-lg"
+        style={{ backgroundImage: `url(${image || thumbnail})` }}
       >
         {/* Hover */}
         <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 rounded-lg">
           <h3 className="w-[80%] text-[20px] font-semibold truncate">
-            {cardData.title}
+            {title}
           </h3>
           {/* 아이콘 */}
           <div className="absolute bottom-6 right-6 flex gap-[18px] text-xl font-normal ">
             <div className="flex gap-1">
-              <img src={iikeIcon} alt="좋아요 아이콘" />
-              <span>{cardData.like}</span>
+              <img src={likeIcon} alt="좋아요 아이콘" />
+              <span>{likes.length}</span>
             </div>
             <div className="flex gap-1">
               <img src={chatIcon} alt="채팅 아이콘" />
-              <span>{cardData.chat}</span>
+              <span>{comments.length}</span>
             </div>
           </div>
         </div>
@@ -47,9 +49,9 @@ export default function ImageCard() {
             IconWidth="w-[28px]"
             IconHeight="h-[28px]"
           />
-          <div className="text-base font-medium">{cardData.name}</div>
+          <div className="text-base font-medium">{author.username}</div>
         </div>
-        <div className="text-base font-light">{cardData.date}</div>
+        <div className="text-base font-light">{date}</div>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
 import SearchBar from "../components/SearchBar";
 import ImageCard from "../components/ImageCard";
 import Tag from "../components/Tag";
+import { useEffect, useState } from "react";
+import { useChannelStore } from "../stores/channelStore";
+import { api } from "../api/axios";
 
 export default function Home() {
+  const channelId = useChannelStore((state) => state.channelId);
+
+  const [posts, setPosts] = useState<PostType[]>([]);
+
+  const getChannelPost = async () => {
+    try {
+      const { data } = await api.get(`/posts/channel/${channelId}`);
+      setPosts(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!channelId) return;
+
+    getChannelPost();
+  }, [channelId]);
+
   return (
     <div className="flex flex-col items-center mt-8 gap-16">
       <div className="flex flex-col items-center gap-[30px] w-full px-4">
@@ -19,25 +41,8 @@ export default function Home() {
       {/* 피드 이미지 */}
       <div className="flex items-center">
         <div className="grid 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 gap-8">
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
-          <ImageCard />
+          {posts.length > 0 &&
+            posts.map((post) => <ImageCard key={post._id} {...post} />)}
         </div>
       </div>
     </div>
