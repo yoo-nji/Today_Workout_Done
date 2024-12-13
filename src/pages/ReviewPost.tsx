2e9e9a5419cc6c65ef60515ea38ca92a34a69329
yoongjeonging <yooong.o353@gmail.com>
2024-12-13 11:22:45 +0900

    feat: 리뷰채널 검색 디바운스 처리

--- src/pages/ReviewPost.tsx
+++ src/pages/ReviewPost.tsx
@@ -4,25 +4,40 @@ import { api } from "../api/axios";
 import SearchBar from "../components/SearchBar";
 import Tag from "../components/Tag";
 import Review from "../components/Review";
+import { useLocation } from "react-router";
 
 export default function ReviewPost() {
+  const location = useLocation();
+  // channelId
   const channelId = useChannelStore((state) => state.channelId);
 
+  // 상태
+  const [status, setStatus] = useState<"idle" | "loading" | "searching">(
+    "idle"
+  );
+
+  // 게시글
   const [posts, setPosts] = useState<PostType[]>([]);
+
+  // 검색 게시글
   const [searchTerm, setSearchTerm] = useState<string>("");
+  // 검색 게시글  저장
   const [searchPosts, setSearchPosts] = useState<PostType[]>([]);
 
+  // 초기 렌더링
+
   const getChannelPost = async () => {
+    setStatus("loading");
     try {
       const { data } = await api.get(`/posts/channel/${channelId}`);
       if (data.length === 0) {
-        console.log("게시물이 없습니다.");
+        console.log("등록된 게시물이 없습니다.");
       }
-
       setPosts(data);
-      setSearchPosts(data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setStatus("idle");
     }
   };
 
@@ -31,17 +46,36 @@ export default function ReviewPost() {
     getChannelPost();
   }, [channelId]);
 
+  // 검색 디바운스 처리
   useEffect(() => {
-    if (searchTerm === "") {
-      setSearchPosts(posts);
-    } else {
-      setSearchPosts(
-        posts.filter((post) =>
-          post.title.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-      );
+    if (!searchTerm) {
+      setStatus("idle");
+      setSearchPosts([]);
+      return;
     }
-  }, [searchTerm, posts]);
+    const searchDebounce = setTimeout(async () => {
+      setStatus("searching");
+      if (searchTerm.length > 0) {
+        try {
+          setStatus("loading");
+          setSearchPosts(
+            posts.filter((post) =>
+              post.title.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+          );
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setStatus("searching");
+        }
+      }
+    }, 1200);
+
+    return () => {
+      clearTimeout(searchDebounce);
+    };
+  }, [searchTerm]);
+
   return (
     <div className="flex flex-col items-center gap-16 mt-8">
       <div className="flex flex-col items-center gap-[30px] w-full px-4">
@@ -60,11 +94,22 @@ export default function ReviewPost() {
       {/* 피드 게시물 */}
       <div className="flex items-center">
         <div>
-          {searchPosts.length > 0 ? (
-            searchPosts.map((post) => <Review key={post._id} {...post} />)
-          ) : (
-            <p>검색 결과가 없습니다.</p>
-          )}
+          {/* 로딩중 */}
+          {status === "loading" && <p>로딩중..</p>}
+
+          {status === "searching" &&
+            (searchPosts.length ? (
+              searchPosts.map((post) => <Review key={post._id} {...post} />)
+            ) : (
+              <p>검색 결과가 없습니다.</p>
+            ))}
+
+          {status === "idle" &&
+            (posts.length ? (
+              posts.map((post) => <Review key={post._id} {...post} />)
+            ) : (
+              <p>" "</p>
+            ))}
         </div>
       </div>
     </div>
