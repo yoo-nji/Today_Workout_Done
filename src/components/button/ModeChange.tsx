87e8ebbca688558eda33bf8182abc19c09f83526
songwon-sung <swsung91@gmail.com>
2024-12-13 10:30:10 +0900

    fix: 다크모드 호버버

--- src/components/button/ModeChange.tsx
+++ src/components/button/ModeChange.tsx
@@ -13,28 +13,28 @@ export default function ModeChange() {
   const [isClicked, setIsClicked] = useState(false);
   const isToggle = usesidebarToggleStore((state) => state.isToggle);
 
-  /* 삭제 모달 테스트용 코드 */
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleClick = () => {
-    // 모달 열기
-    setIsModalOpen(true);
-  };
-
-  const handleModalClose = () => {
-    // 모달 닫기
-    setIsModalOpen(false);
-  };
-
-  const handleModeChange = () => {
-    // 실제 모드 변경 로직
-    setIsClicked((click) => !click);
-    setIsModalOpen(false);
-  };
-  /* 테스트용 코드 여기까지 */
-
+  // /* 삭제 모달 테스트용 코드 */
+  // const [isModalOpen, setIsModalOpen] = useState(false);
   // const handleClick = () => {
-  //   setIsClicked((click) => !click);
+  //   // 모달 열기
+  //   setIsModalOpen(true);
   // };
+
+  // const handleModalClose = () => {
+  //   // 모달 닫기
+  //   setIsModalOpen(false);
+  // };
+
+  // const handleModeChange = () => {
+  //   // 실제 모드 변경 로직
+  //   setIsClicked((click) => !click);
+  //   setIsModalOpen(false);
+  // };
+  // /* 테스트용 코드 여기까지 */
+
+  const handleClick = () => {
+    setIsClicked((click) => !click);
+  };
 
   return (
     <div>
@@ -42,7 +42,7 @@ export default function ModeChange() {
       <button
         onClick={handleClick}
         className={twMerge(
-          "relative w-[100px] h-[40px] rounded-full text-white font-bold text-[16px] overflow-hidden",
+          "relative w-[100px] h-[40px] rounded-full text-white font-bold text-[16px] overflow-hidden group",
           !isToggle && "hidden"
         )}
         style={{
@@ -56,18 +56,19 @@ export default function ModeChange() {
         <img
           src={isClicked ? moon : sun}
           alt="circleHorizontal"
-          className="absolute w-[30px] h-[30px] rounded-full transition-all duration-300 ease-in-out"
-          style={{
-            transform: isClicked ? "translateX(5px)" : "translateX(65px)",
-            marginTop: "-15px",
-            top: "50%",
-          }}
+          className={twMerge(
+            `absolute w-[30px] h-[30px] rounded-full transition-all duration-300 ease-in-out ${
+              isClicked ? "translate-x-[5px]" : "translate-x-[65px]"
+            } mt-[-15px] top-[50%] group-hover:scale-75`
+          )}
         />
       </button>
+
+      {/* 사이드바 닫혔을 때 다크모드 버튼 */}
       <button
         onClick={handleClick}
         className={twMerge(
-          "relative w-[40px] h-[100px] rounded-full text-white font-bold text-[16px] overflow-hidden",
+          "relative w-[40px] h-[100px] rounded-full text-white font-bold text-[16px] overflow-hidden group",
           isToggle && "hidden"
         )}
         style={{
@@ -81,24 +82,22 @@ export default function ModeChange() {
         <img
           src={isClicked ? moon : sun}
           alt="circleVertical"
-          className="absolute w-[30px] h-[30px] rounded-full transition-all duration-300 ease-in-out"
-          style={{
-            transform: isClicked ? "translateY(65px)" : "translateY(5px)",
-            marginTop: "0px",
-            top: "0%",
-            right: "12%",
-          }}
+          className={twMerge(`absolute w-[30px] h-[30px] rounded-full transition-all duration-300 ease-in-out 
+            ${
+              isClicked ? "translate-y-[65px]" : "translate-y-[5px]"
+            } mt-0 top-0 right-[12%]  group-hover:scale-75`)}
         />
       </button>
-      {/* DeleteConfirm 모달 */}
-      {isModalOpen && (
+
+      {/* 테스트용 DeleteConfirm 모달 */}
+      {/* {isModalOpen && (
         <DeleteConfirm
           isOpen={isModalOpen}
           onClose={handleModalClose}
           onConfirm={handleModeChange}
           message="이 게시글을 정말 삭제하시겠습니까?"
         />
-      )}
+      )} */}
     </div>
   );
 }
