a6f28ad4e6c66e9ce62fedddd73243706296011f
songwon-sung <swsung91@gmail.com>
2024-12-18 15:50:44 +0900

    feat: 다크모드 error 페이지지

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -5,16 +5,18 @@ export default function Error() {
   return (
     <div className="flex flex-col h-screen ">
       <Header logo />
-      <div className="flex flex-grow items-center justify-center bg-[#F2F3F4]">
+      <div className="flex flex-grow items-center justify-center bg-[#F2F3F4] dark:bg-lightBlackDark dark:border-blackDark">
         <div className="text-center ">
-          <h1 className="text-6xl font-bold text-[#265CAC] font-jalnan">404</h1>
-          <h1 className="text-4xl font-bold mt-4 text-[#265CAC] font-jalnan">
+          <h1 className="text-6xl font-bold text-[#265CAC] font-jalnan dark:text-pinkDark">
+            404
+          </h1>
+          <h1 className="text-4xl font-bold mt-4 text-[#265CAC] font-jalnan dark:text-mainTextDark">
             죄송합니다
           </h1>
-          <p className="text-xl font-medium mt-6 [#272727] font-jalnan">
+          <p className="text-xl font-medium mt-6 [#272727] font-jalnan dark:text-greyDark">
             요청하신 정보가 변경되거나 삭제되어
           </p>
-          <p className="text-xl font-medium mt-4 [#272727] font-jalnan">
+          <p className="text-xl font-medium mt-4 [#272727] font-jalnan dark:text-greyDark">
             정해진 루틴대로 운동을 하지 못하고 있습니다...
           </p>
           <div className="flex h-[300px] justify-center mt-4">
