29a745242fcae72becf0b3dc12a70bad2a17b59b
yoonji <kyjkbw@gmail.com>
2024-12-18 11:14:11 +0900

    feat: 메인 데이터 필터링

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,12 +5,29 @@ import BmiCon from "../components/main/BmiCon";
 
 export default function Main() {
   const [posts, setPosts] = useState<PostType[]>([]);
+  const [proteinPosts, setProteinPosts] = useState<PostType[]>([]);
+  const [routinePosts, setRoutinePosts] = useState<PostType[]>([]);
+  const [gymreviewPosts, setGymreviewPosts] = useState<PostType[]>([]);
 
+  //TODO: 반복 작업 수정 필요
   const getChannelPost = async () => {
     try {
-      const { data } = await api.get("/posts/channel/675a2e0d0d335f0ddae3a194");
-      // console.log(data);
-      setPosts(data);
+      const { data: posts } = await api.get(
+        "/posts/channel/675a2e0d0d335f0ddae3a194"
+      );
+      const { data: proteinPosts } = await api.get(
+        "/posts/channel/675a2dac0d335f0ddae3a188"
+      );
+      const { data: routinePosts } = await api.get(
+        "/posts/channel/675a2dc40d335f0ddae3a18c"
+      );
+      const { data: gymreviewPosts } = await api.get(
+        "/posts/channel/675a2ddc0d335f0ddae3a190"
+      );
+      setPosts(posts.slice(0, 10));
+      setProteinPosts(proteinPosts.slice(0, 10));
+      setRoutinePosts(routinePosts.slice(0, 10));
+      setGymreviewPosts(gymreviewPosts.slice(0, 10));
     } catch (err) {
       console.error(err);
     }
@@ -22,9 +39,9 @@ export default function Main() {
 
   const category = [
     { title: "👋 오운완 인증 최신글!", body: posts },
-    { title: "✅ 루틴 공유 최신글!", body: posts },
-    { title: "💪 프로틴 추천 최신글!", body: posts },
-    { title: "🏋️‍♂️ 헬스장 후기 최신글!", body: posts },
+    { title: "✅ 루틴 공유 최신글!", body: proteinPosts },
+    { title: "💪 프로틴 추천 최신글!", body: routinePosts },
+    { title: "🏋️‍♂️ 헬스장 후기 최신글!", body: gymreviewPosts },
   ];
 
   return (
