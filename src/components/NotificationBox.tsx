d01c85723661811ff8e602de5b3f5e7247e3c6f6
songwon-sung <swsung91@gmail.com>
2024-12-18 18:34:06 +0900

    feat: 다크모드 알림창창

--- src/components/NotificationBox.tsx
+++ src/components/NotificationBox.tsx
@@ -14,10 +14,10 @@ export default function NotificationBox({
   notificationType,
 }: NotificationBoxPropsType) {
   return (
-    <div className="w-[320px] h-[75px] bg-[#EFEFEF] mt-4 flex items-center gap-3 pl-[20px] rounded-[10px] flex-shrink-0 ">
+    <div className="w-[320px] h-[75px] bg-[#EFEFEF] mb-[5px] flex items-center gap-3 pl-[20px] rounded-[10px] flex-shrink-0 dark:bg-darkGreyDark dark:text-greyDark">
       {/* 유저 프로필, 현활 */}
       <div
-        className="bg-white w-[48px] h-[48px] flex justify-center items-center rounded-[50%] shadow-inner cursor-pointer relative"
+        className="bg-white w-[48px] h-[48px] flex justify-center items-center rounded-[50%] shadow-inner cursor-pointer relative dark:bg-greyDark"
         // onClick={() => handleClick(userid)}
       >
         <img
@@ -28,7 +28,7 @@ export default function NotificationBox({
       </div>
 
       {notificationType === "follow" && (
-        <div className=" w-[220px] h-[50px] items-center bg">
+        <div className=" w-[220px] h-[50px] flex items-center justify-center">
           <article className="text-xs font-bold">
             {fullname} 님께서 회원님을 팔로우합니다!
           </article>
@@ -36,14 +36,14 @@ export default function NotificationBox({
         </div>
       )}
       {notificationType === "comment" && (
-        <div className="w-[220px] h-[50px] ">
+        <div className="w-[220px] h-[50px] flex items-center justify-center">
           <p className="text-xs font-bold">
             {fullname} 님께서 회원님의 글에 댓글을 남겼습니다{" "}
           </p>
         </div>
       )}
       {notificationType === "like" && (
-        <div className=" w-[230px] h-[50px]">
+        <div className=" w-[230px] h-[50px] flex items-center justify-center">
           <p className="text-xs font-bold ">
             {fullname} 님께서 회원님께서 글에 좋아요를 눌러주셨습니다
           </p>
