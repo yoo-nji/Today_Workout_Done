da2c2c0f6771d69b3504a32aae67214aff0754ba
kkyuheak <gyuheak@gmail.com>
2024-12-19 12:40:03 +0900

    FEAT 메인페이지 다크모드 구현

--- src/components/main/SwiperCustom.tsx
+++ src/components/main/SwiperCustom.tsx
@@ -1,8 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Virtual } from "swiper/modules";
 import ImageCard from "../ImageCard";
+import { useDarkModeStore } from "../../stores/darkModeStore";
 
 export default function SwiperCustom({ posts }: { posts: PostType[] }) {
+  const isDark = useDarkModeStore((state) => state.isDark);
+
   return (
     <div className="mb-[60px]">
       <Swiper
@@ -21,6 +24,7 @@ export default function SwiperCustom({ posts }: { posts: PostType[] }) {
           1536: { slidesPerView: 4 }, // 큰 화면
         }}
         virtual
+        className={!isDark ? "" : "dark"}
       >
         {posts.map((post) => (
           <SwiperSlide key={post._id}>
@@ -30,7 +34,7 @@ export default function SwiperCustom({ posts }: { posts: PostType[] }) {
           </SwiperSlide>
         ))}
         <div className="swiper-pagination-box">
-          <div className="swiper-pagination"></div>
+          <div className="swiper-pagination dark:bg-[#5e5e5e] dark:text-white"></div>
         </div>
       </Swiper>
     </div>
