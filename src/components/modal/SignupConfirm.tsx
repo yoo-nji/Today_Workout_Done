82d5b688c5f7159ea9933463ac69f112fa6a52b7
kkyuheak <gyuheak@gmail.com>
2024-12-23 11:08:19 +0900

    FIX 회원가입 완료 모달 다크모드 적용

--- src/components/modal/SignupConfirm.tsx
+++ src/components/modal/SignupConfirm.tsx
@@ -8,15 +8,17 @@ export function SignupConfirm({ setIsModalOpen }: SignupConfirmProps) {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-10 rounded-md text-center font-medium">
-        <p className="text-lg mb-4">회원가입이 완료되었습니다.</p>
+    <div className="fixed top-0 left-0 w-full h-full bg-black dark:bg-darkGreyDark  bg-opacity-50 dark:bg-opacity-80 flex justify-center items-center">
+      <div className="bg-white p-10 rounded-md text-center font-medium dark:bg-blackDark">
+        <p className="text-lg mb-4 dark:text-white">
+          회원가입이 완료되었습니다.
+        </p>
         <button
           onClick={() => {
             setIsModalOpen(false);
             navigate("/login");
           }}
-          className="px-4 py-2 bg-[#265CAC] text-white rounded-md"
+          className="px-4 py-2 bg-[#265CAC] dark:bg-mainDark text-white dark:text-blackDark rounded-md"
         >
           확인
         </button>
